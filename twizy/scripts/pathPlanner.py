1e3724089f274705f5b63ba2db0736eb88d2fe32
Sam <samneme55@gmail.com>
2020-03-17 23:21:51 +0100

    _

--- twizy/scripts/pathPlanner.py
+++ twizy/scripts/pathPlanner.py
@@ -10,12 +10,6 @@ class Coordinate:
       self.x = x
       self.y = y
 
-    def getX(self):
-        return self.x
-
-    def getY(self):
-        return self.y
-
 
 def makeMap():      ## for now it will return a fixed map
     """This method creates a dictionary by combining the GPS-coordinate and
@@ -28,39 +22,69 @@ def makeMap():      ## for now it will return a fixed map
         0.4: 1,
         0.5: 1,
         0.6: 1,
-        0.7: 2,
-        0.8: 2,
-        0.9: 2,
-        1.0: 2,
-        1.1: 2,
-        1.2: 2,
-        1.3: 2,
-        1.4: 2,
-        1.5: 2,
-        1.6: 2,
-        1.7: 2,
-        1.8: 2,
-        1.9: 2,
-        2.0: 2,
-        2.1: 2,
-        2.2: 2,
-        2.3: 2,
-        2.5: 2,
-        2.6: 2,
-        2.7: 2,
-        2.8: 2,
-        2.9: 2,
-        3.0: 2,
-        3.1: 2,
-        3.2: 2,
-        3.3: 2,
-        3.4: 2,
-        3.5: 1,
-        3.6: 1,
-        3.7: 1,
-        3.8: 1,
-        3.9: 1,
-        4.0: 1
+        0.7: 3.5,
+        0.8: 3.5,
+        0.9: 3.5,
+        1.0: 3.5,
+        1.1: 3.5,
+        1.2: 3.5,
+        1.3: 3.5,
+        1.4: 3.5,
+        1.5: 3.5,
+        1.6: 3.5,
+        1.7: 3.5,
+        1.8: 3.5,
+        1.9: 3.5,
+        2.0: 3.5,
+        2.1: 3.5,
+        2.2: 3.5,
+        2.3: 3.5,
+        2.5: 3.5,
+        2.6: 3.5,
+        2.7: 3.5,
+        2.8: 3.5,
+        2.9: 3.5,
+        3.0: 3.5,
+        3.1: 3.5,
+        3.2: 3.5,
+        3.3: 3.5,
+        3.4: 3.5,
+        3.5: 3.5,
+        3.6: 3.5,
+        3.7: 3.5,
+        3.8: 3.5,
+        3.9: 3.5,
+        4.0: 3.5,
+        4.1: 3.5,
+        4.2: 3.5,
+        4.3: 3.5,
+        4.4: 3.5,
+        4.5: 3.5,
+        4.6: 3.5,
+        4.7: 3.5,
+        4.8: 3.5,
+        4.9: 3.5,
+        5.0: 3.5,
+        5.1: 3.5,
+        5.2: 3.5,
+        5.3: 3.5,
+        5.4: 3.5,
+        5.5: 3.5,
+        5.6: 3.5,
+        5.7: 3.5,
+        5.8: 3.5,
+        5.9: 3.5,
+        6.0: 3.5,
+        6.1: 1.0,
+        6.2: 1.0,
+        6.3: 1.0,
+        6.4: 1.0,
+        6.5: 1.0,
+        6.6: 1.0,
+        6.7: 1.0,
+        6.8: 1.0,
+        6.9: 1.0,
+        7.0: 1.0,
     }
     return parkmap
 
@@ -85,50 +109,65 @@ def tangent_line(f, x_0, a, b):
     plt.title('Plot of a function with tangent line')
     plt.show()
 
-def ff(a, b, c, t):
-    return a*np.arctan(c) + a*np.arctan(t/b - c)
+def f_arctan(a, b, c, x):
+    return a*np.arctan(c) + a*np.arctan(x/b - c)
 
 
+def f_arctan_d1(a, b, c, x):        #derivative of f_arctan
+    return a/(b*(1+(np.power(c-x/b,2))))
 
-def tanfunc(current,goal):
+
+def f_arctan_d2(a, b, c, x):        #second derivative of f_arctan
+    return (2*a*(c-x/b)) / (np.power(b,2)*(np.power(np.power(c-x/b,2)+1,2)))
+
+
+def path(current,goal):
     """first argument takes in the current coordinate of the car and the second is the
      coordinate of the goal position this method will return the optimal
      trajectory"""
     CarLength = 3
-    Depth =  2       #np.abs(goal.getY - current.getY)
-    Length = 10     #np.abs(goal.getX - current.getX)
-    Period = 5      # changes the slope of the curve, temporary**
+    Depth =  np.abs(goal.y - current.y)    #2
+    Length = np.abs(goal.x - current.y)      #10
+    Period = 7      # changes the slope of the curve, temporary**
     Deptharray = np.linspace(0,Depth,20)
     Periodarray =np.linspace(0, Period,20)
     Lengtharray = np.linspace(0,Length,20)
-    Phasearray = np.linspace(1,3,10)
+    Phasearray = np.linspace(1,5,20)
 
     for a in Deptharray:           #????
         for b in Periodarray:        #????
             for c in Phasearray:
                 if a != 0 and b!= 0:
-                    p1 = a - CarLength*np.cos((a*b)/(pow(b, 2)))  # Need to add x
-                    #linspace that envelopes the whole car
-                    #creating a tangent line
-                    function = ff(a, b, c, Lengtharray)
+                    function= f_arctan(a, b, c, Lengtharray)
+
+                    if abs((f_arctan(a, b, c, goal.x) - goal.y)) > 0.2:   # filter out every function of which distance to
+                        break                                             # to the goal position at goal.x is to big
+
+                    if f_arctan_d1(a, b, c, goal.x ) > 0.1:               # filter out every function that ends with a slope
+                        break                                             # larger than 0.1 rad (to ensure that the car is parked horizontally)
+                    for x in Lengtharray:       # filter out every function with a radius of curvature larger
+                       if x != 0:               # than that of renault twizy
+
+                           f_deriv = f_arctan_d1(a, b, c, x)        # first derivative
+                           f_deriv2 = f_arctan_d2(a, b, c, x)       # second derivative
+
+                           if f_deriv2 != 0:
+                               Radius= int(np.absolute(np.power(1+np.power(f_deriv, 2), 3/2)/((f_deriv2))))  #formula for radius of curvature
+                               if Radius < 3.3:
+                                  plt.plot(Lengtharray, function)
+                                  #TODO: Lägg till dessa i funktioner
 
 
 
-                    #Radius = np.power(func_d)+1
-                    #Radius= np.power(1+np.power(func_d,2), 3/2)/(np.absolute(func_d2))     # formula for radius of curvature
-                    #if Radius.any() > 0.6981 or Radius.any() < -0.6981:
-                    #   print(Radius)
 
-                    #   break
-                    plt.plot(Lengtharray, function)
 
 
 
 
 current = Coordinate(0,0)
-goal = Coordinate(4,2)
+goal = Coordinate(5, 2.25)
 
-a=2.1
+a=2.5
 b=1.4
 c=1
 CarLength=3
@@ -137,5 +176,5 @@ CarLength=3
 plt.plot(list(makeMap().keys()),list(makeMap().values()))
 #plt.show()
 
-tanfunc(current, goal)
+path(current, goal)
 plt.show()
